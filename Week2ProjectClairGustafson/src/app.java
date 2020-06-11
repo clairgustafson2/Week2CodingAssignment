a5e5f2062fd9fe271acf5e14453f8d620c25a7f4
clairgustafson <clairgustafson@localhost>
2020-06-11 10:38:44 -0700

    Commit 1

---
+++ Week2ProjectClairGustafson/src/app.java
@@ -0,0 +1,24 @@
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Step 1. Boolean expressions
+		System.out.println(true && false);
+		System.out.println(true || false);
+		System.out.println(false && false);
+		System.out.println(true && (false || true));
+		System.out.println(false || (true && false));
+		System.out.println(false || 1 < 5);
+		System.out.println(5 >= 4 && 1 > 3);
+		System.out.println(10 < 4 || 1 > 4);
+		System.out.println(12 >= 2 && 1 < 24);
+		
+		String itIsHotOutside = "True";
+		
+		
+
+	}
+
+}
