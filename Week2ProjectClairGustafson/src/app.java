4f4708c00af3ea4bd180c06063aaa25781e634d2
clairgustafson <clairgustafson@localhost>
2020-06-11 13:23:00 -0700

    Commit 2

--- Week2ProjectClairGustafson/src/app.java
+++ Week2ProjectClairGustafson/src/app.java
@@ -14,9 +14,33 @@ public class app {
 		System.out.println(5 >= 4 && 1 > 3);
 		System.out.println(10 < 4 || 1 > 4);
 		System.out.println(12 >= 2 && 1 < 24);
+		//“Hello”.charAt(0) == ‘h’ Having issues with this line of code
 		
-		String itIsHotOutside = "True";
+		// Step 2. Boolean variables
+		Boolean itIsHotOutside = true;
+		Boolean isWeekday = true;
+		Boolean hasMoneyInPocket = false;
+		System.out.println(itIsHotOutside);
+		System.out.println(isWeekday);
+		System.out.println(hasMoneyInPocket);
 		
+		//Step 3. Create variables
+		double costOfMilk = 4.89;
+		double moneyInWallet = 19.34;
+		int thirstLevel = 4;
+		System.out.println(costOfMilk);
+		System.out.println(moneyInWallet);
+		System.out.println(thirstLevel);
+		
+		//Step 4. Create variables for the following scenarios
+		boolean shouldBuyIcecream = itIsHotOutside && hasMoneyInPocket;
+		boolean willGoSwimming = itIsHotOutside && isWeekday;
+		boolean isAGoodDay = itIsHotOutside && hasMoneyInPocket && isWeekday;
+		boolean willBuyMilk = itIsHotOutside && thirstLevel >=3 && moneyInWallet >= 2*costOfMilk;
+		System.out.println(shouldBuyIcecream);
+		System.out.println(willGoSwimming);
+		System.out.println(isAGoodDay);
+		System.out.println(willBuyMilk);
 		
 
 	}
