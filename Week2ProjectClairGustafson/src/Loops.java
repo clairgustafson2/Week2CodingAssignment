ef3b71375875a35ba41929044d75dcc5becb8fbc
clairgustafson <clairgustafson@localhost>
2020-06-11 17:23:38 -0700

    Commit 4

--- Week2ProjectClairGustafson/src/Loops.java
+++ Week2ProjectClairGustafson/src/Loops.java
@@ -9,20 +9,20 @@ public class Loops {
 		//prints all even numbers between 0 to 100
 		int x = 0;
 		while (x <= 50) {
-			System.out.println(x * 2);
+			System.out.print(x * 2 + " ");
 			x++;
 		}
 		
 		//print every 3rd number going backwards from 100 to 0
 		int y = 100;
 		while (y >= 0) {
-			System.out.println(y);
+			System.out.print(y + " ");
 			y -= 3;
 		}
 		
 		//prints every other number from 1 to 100
 		for (int i = 1; i <= 100; i += 2) {
-			System.out.println(i);
+			System.out.print(i + " ");
 		}
 		
 		/*print every number from 0 to 100, but if the number is divisible by 3, it prints “Hello” instead of the number, 
@@ -31,13 +31,13 @@ public class Loops {
 		
 		for (int i=1; i<=100; i++) {
 			if (i%3==0) {
-				System.out.println("Hello ");
-			} if (i%5==0) {
-				System.out.println("World ");
-			} if (i%3==0 && i%5==0) {
-				System.out.println("HelloWorld ");
+				System.out.print("Hello ");
+			} else if (i%5==0) {
+				System.out.print("World ");
+			} else if (i%3==0 && i%5==0) {
+				System.out.print("HelloWorld ");
 			} else {
-				System.out.println(i + " ");
+				System.out.print(i + " ");
 			}
 						
 		}			
